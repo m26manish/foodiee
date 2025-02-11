4ce7e0ab2e13f6acc48824e2b28110892cc4c828
manish yadav <manishyadav@manishs-Laptop.local>
2025-02-11 16:24:44 +0530

    first commit

---
+++ navigation/appNavigation.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const appNavigation = () => {
+  return (
+    <View>
+      <Text>appNavigation</Text>
+    </View>
+  )
+}
+
+export default appNavigation
+
